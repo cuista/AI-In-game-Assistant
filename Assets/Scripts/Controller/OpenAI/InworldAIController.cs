2f48d94b27ee5b972f8601c3bccce8f9bfcfc594
Carlo Cristarella Orestano <carlo.cristarella@iei.local>
2023-10-20 17:45:42 +0200

    First integration of AI using the plugin Inworld AI Unity SDK, with working prototype of AI Ingame Assistant: Echo

---
+++ Assets/Scripts/Controller/OpenAI/InworldAIController.cs
@@ -0,0 +1,37 @@
+using Inworld.Sample;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InworldAIController : MonoBehaviour
+{
+    public InworldPlayer2D player;
+    public TMP_InputField message;
+    private bool _isFirstSentence = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Chat();
+    }
+
+    public void Chat() { StartCoroutine(AIPrompting()); }
+
+    IEnumerator AIPrompting()
+    {
+        yield return new WaitForSeconds(2);
+        if (_isFirstSentence)
+        {
+            message.text = "Generete a response saying 'Hello, I'm Echo' and then giving an introduction of the prologue to the player with sarcasm. Answer in italian.";
+            player.SendText();
+            _isFirstSentence = false;
+            message.text = "Generete a response telling more to the player or giving help to the player or saying something sarcastic. Answer in italian.";
+        }
+        else
+        {
+            player.SendText();
+        }
+    }
+}
