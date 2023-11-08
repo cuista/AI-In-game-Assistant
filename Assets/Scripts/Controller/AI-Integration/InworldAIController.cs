538e90d4721f42a658836d1ac7355c5f8909910e
Carlo Cristarella Orestano <carlo.cristarella@iei.local>
2023-11-08 16:10:19 +0100

    Added a trigger timer if player get stack on a particular goal. Fixed echo in the gameplay scene.

--- Assets/Scripts/Controller/AI-Integration/InworldAIController.cs
+++ Assets/Scripts/Controller/AI-Integration/InworldAIController.cs
@@ -1,4 +1,5 @@
 using Ai.Inworld.Studio.V1Alpha;
+using Google.Protobuf.WellKnownTypes;
 using Inworld;
 using Inworld.Sample;
 using Inworld.Util;
@@ -19,16 +20,23 @@ public class InworldAIController : MonoBehaviour
     //A dictionary that maps plot events to trigger events
     private List<string> levelTriggers = new List<string>();
 
+    //To handle expired trigger
+    public float currentTriggerDuration;
+    private float currentTriggerTime;
+
     // Start is called before the first frame update
     void Start()
     {
         player = (player==null)?FindObjectOfType<InworldPlayer2D>():player;
         echo = (echo == null) ? FindObjectOfType<InworldCharacter>() : echo;
         LoadLevelTriggers();
+        currentTriggerTime = 0;
+        currentTriggerDuration = 45f;
     }
 
     private void FixedUpdate()
     {
+        currentTriggerTime += Time.fixedDeltaTime;
         Invoke("EchoTriggering",2f);
     }
 
@@ -44,30 +52,39 @@ public class InworldAIController : MonoBehaviour
     private void EchoTriggering()
     {
         float posZ = player.transform.position.z;
-        if (_isFirstSentence && posZ < 76f)
+        if (currentTriggerTime < currentTriggerDuration)
         {
-            // Trigger the level event
-            echo.SendTrigger(levelTriggers[0]);
-            Debug.Log("COMPLETED GOAL:" + GetNextPuzzleEvent());
-            _isFirstSentence = false;
+            if (_isFirstSentence && posZ < 76f)
+            {
+                // Trigger the level event
+                echo.SendTrigger(levelTriggers[0]);
+                Debug.Log("COMPLETED GOAL:" + GetNextPuzzleEvent());
+                _isFirstSentence = false;
+            }
+            else
+            {
+                if (levelTriggers[0] == "put_clone_over_button" && posZ >= 76f)
+                {
+                    Debug.Log("COMPLETED GOAL:" + GetNextPuzzleEvent());
+                    echo.SendTrigger(levelTriggers[0]);
+                }
+                else if (levelTriggers[0] == "overcome_first_door" && posZ >= 102f)
+                {
+                    Debug.Log("COMPLETED GOAL:" + GetNextPuzzleEvent());
+                    echo.SendTrigger(levelTriggers[0]);
+                }
+                else if (levelTriggers[0] == "prototype_level_completed")
+                {
+                    _isFirstSentence = true;
+                    LoadLevelTriggers();
+                }
+            }
         }
-        else
+        else //CurrentTriggerTimer is expired
         {
-            if (levelTriggers[0] == "put_clone_over_button" && posZ >= 76f)
-            {
-                Debug.Log("COMPLETED GOAL:" + GetNextPuzzleEvent());
-                echo.SendTrigger(levelTriggers[0]);
-            }
-            else if (levelTriggers[0] == "overcome_first_door" && posZ >= 102f)
-            {
-                Debug.Log("COMPLETED GOAL:" + GetNextPuzzleEvent());
-                echo.SendTrigger(levelTriggers[0]);
-            }
-            else if (levelTriggers[0] == "prototype_level_completed")
-            {
-                _isFirstSentence = true;
-                LoadLevelTriggers();
-            }
+            Debug.Log("ECHO TRIGGERED TIMER EXPIRED");
+            echo.SendTrigger(levelTriggers[0]); //After duration time echo is triggered again
+            currentTriggerTime = 0;
         }
     }
 
