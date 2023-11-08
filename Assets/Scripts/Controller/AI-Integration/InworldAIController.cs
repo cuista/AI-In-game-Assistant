6357f683110c7f9c5f68aa7c33f5514ea324f87e
Carlo Cristarella Orestano <carlo.cristarella@iei.local>
2023-11-08 12:38:50 +0100

    Implemented some sketchy triggers to InworldAIController to handle events in game and let AI helps the player.

--- Assets/Scripts/Controller/AI-Integration/InworldAIController.cs
+++ Assets/Scripts/Controller/AI-Integration/InworldAIController.cs
@@ -1,7 +1,10 @@
+using Ai.Inworld.Studio.V1Alpha;
 using Inworld;
 using Inworld.Sample;
+using Inworld.Util;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,38 +16,67 @@ public class InworldAIController : MonoBehaviour
     public TMP_InputField message;
     private bool _isFirstSentence = true;
 
+    //A dictionary that maps plot events to trigger events
+    private List<string> levelTriggers = new List<string>();
+
     // Start is called before the first frame update
     void Start()
     {
-        Chat();
+        player = (player==null)?FindObjectOfType<InworldPlayer2D>():player;
+        echo = (echo == null) ? FindObjectOfType<InworldCharacter>() : echo;
+        LoadLevelTriggers();
     }
 
-    public void Chat() { StartCoroutine(AIPrompting()); }
-
-    IEnumerator AIPrompting()
+    private void FixedUpdate()
     {
-        yield return new WaitForSeconds(2);
-        if (_isFirstSentence)
+        Invoke("EchoTriggering",2f);
+    }
+
+    public void LoadLevelTriggers()
+    {
+        // Initialize level events triggers
+        levelTriggers.Add("prototype_level_started");
+        levelTriggers.Add("put_clone_over_button");
+        levelTriggers.Add("overcome_first_door");
+        levelTriggers.Add("prototype_level_completed");
+    }
+
+    private void EchoTriggering()
+    {
+        float posZ = player.transform.position.z;
+        if (_isFirstSentence && posZ < 76f)
         {
-            message.text = "Generete a response saying 'Hello, I'm Echo' and then giving an introduction of the prologue to the player with sarcasm. Answer in italian.";
-            player.SendText();
+            // Trigger the level event
+            echo.SendTrigger(levelTriggers[0]);
+            Debug.Log("COMPLETED GOAL:" + GetNextPuzzleEvent());
             _isFirstSentence = false;
-            message.text = "Generete a response telling more to the player or giving help to the player or saying something sarcastic. Answer in italian.";
         }
         else
         {
-            player.SendText();
-            echo.SendTrigger("prototype_level_started");
+            if (levelTriggers[0] == "put_clone_over_button" && posZ >= 76f)
+            {
+                Debug.Log("COMPLETED GOAL:" + GetNextPuzzleEvent());
+                echo.SendTrigger(levelTriggers[0]);
+            }
+            else if (levelTriggers[0] == "overcome_first_door" && posZ >= 102f)
+            {
+                Debug.Log("COMPLETED GOAL:" + GetNextPuzzleEvent());
+                echo.SendTrigger(levelTriggers[0]);
+            }
+            else if (levelTriggers[0] == "prototype_level_completed")
+            {
+                _isFirstSentence = true;
+                LoadLevelTriggers();
+            }
         }
     }
 
-    public void TriggerSent()
+    private string GetNextPuzzleEvent()
     {
-        Debug.Log("Trigger sent");
-    }
+        //Get the first available hint from the puzzle hints dictionary
+        string levelEvent = levelTriggers[0];
+        levelTriggers.RemoveAt(0);
 
-    public void TriggerReceived()
-    {
-        Debug.Log("Trigger received");
+        return levelEvent;
     }
 }
